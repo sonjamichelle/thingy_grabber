c2ddd6afddd45c80bc861659836e20d8cbba678b
Sonja M Thomas <sonja@designcreations.us>
2022-06-23 06:02:19 -0500

    MyFork
    My Fork

---
+++ get_my_collections.bat
@@ -0,0 +1,63 @@
+ECHO Getting Thingiverse Collections
+
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Ender 3 Max"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Planes"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Jewelery"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Armed Services"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "GlowForge"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Craft Stuff"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Ender 3 V2"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Stargate"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Robotech"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Star Wars"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Babylon 5"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Star Trek"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Orion Toys"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Pi Stuff"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "DC_Stuff"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Workshop Stuff"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "HAM Radio"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "FlightSim"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Nikki Stuff"
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" collection sonjadc "Things to Make"
+
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user mathgrrl
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user alpokemon
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user igniz
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user adafruit
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user dragonflyfabrication
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user sejk
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user FlightSimMaker
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user HatsuneR
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user skimbal
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user usagi3
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user m3dm
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user printedobsession
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user fase-2
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user stormcrow13
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user rocketpiggames
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user misterchiefcostuming
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user jace1969
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user archandachxs
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user moesizzlac
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user larry009
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user oddworks
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user davidbogaerts
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user ljbeng
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user edy209
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user shaggydude
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user kabak
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user konchan77
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user robotechmaster
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user dedjoshua
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user caj
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user technoviper3d
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user kabrumble
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user depronized
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user chrism112
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user openair
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user sablebadger
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user kevinmakescoolthings
+START "" "D:\thingy_grabber\thingy_grabber.exe" -a f2cc5dd652fb3ef1b3082461d612f391 -d "\\192.168.1.2\3D_Files\3D Print Files\a-Thingiverse_Collections" user rrwerft
+
+ECHO Done!
